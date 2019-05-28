64fbbb4ba2c8ff2eb9e5961ab9721231b2e4c15d
Alexander Schelchere <ascellie@gmail.com>
2019-05-28 16:00:35 -0400

    apply coupons - complete

--- grocer.rb
+++ grocer.rb
@@ -5,7 +5,7 @@ def consolidate_cart(cart)
   cart.map do |groceries|
 
     groceries.map do |item, details|
-      
+
       if !neat_cart.has_key?(item)
         neat_cart[item] = details
         neat_cart[item][:count] = 1
@@ -20,7 +20,56 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  clipped_cart = {}
+
+  cart.map do |item, details|
+    # the array holds the coupons (hash)
+
+    coupons.map do |coupon|
+      # coupon[:item, :num, :cost], value
+        
+      # if the cart's "item" matches the coupon's :item => value
+      if item == coupon[:item]
+        # make a string for the item with discount. This will become our key.
+        clipped_item = "#{item} W/COUPON"
+        # based on the :count in the cart and the :num on the coupon. 
+        if details[:count] >= coupon[:num]
+          
+          # We'll create a new hash
+          clipped_cart[clipped_item] = {
+            :price => coupon[:cost],
+            :clearance => details[:clearance],
+            :count => details[:count] / coupon[:num]
+          }
+          # In the event that we have a leftover, said item won't be discounted.
+          # That leftover will get it's count based on the remainder of count, 
+          # and the number the coupon takes(Modulo)
+          remainder = details[:count] % coupon[:num]
+          cart.delete(item) if remainder == 0
+          
+          if remainder > 0
+            cart[item] = {
+              :price => details[:price],
+              :clearance => details[:clearance],
+              :count => remainder
+            }
+          end
+          
+        end
+        
+      end
+        
+    end
+    
+  end
+  # if any coupons were applied
+  if clipped_cart.length > 0
+  cart.merge!(clipped_cart)
+  else
+    # if No coupons were applied
+    cart
+  end
+
 end
 
 def apply_clearance(cart)
